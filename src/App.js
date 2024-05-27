41f323993f2dc408f0979dc7207c1b0cd7dcfbcb
SHWETA <shwetarana072802@gmail.com>
2024-05-27 08:59:37 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://github.com/5hweta28/Weather_Application">
+          Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a href="https://www.linkedin.com/in/shweta-rana-sr04/">
+          Shweta Rana
+        </a>{" "}
+      </div>
+    </React.Fragment>
   );
 }
 
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    key: "ac399df6ef88faf996d8146fb614900d",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
